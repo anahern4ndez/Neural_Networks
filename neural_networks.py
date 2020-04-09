7ddee77f9fc9a51e00ca568945fef24b4639b0e3
Ana Hernandez <her17138@uvg.edu.gt>
2020-04-09 00:29:10 -0600

    back prop implementartion ready to test

--- neural_networks.py
+++ neural_networks.py
@@ -5,6 +5,20 @@
 '''
 from back_propagation import *
 import numpy as np
+import mnist_reader
+
+# data import 
+X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
+X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
+
+m,n = X_train.shape
+HIDDEN_LAYER = 130 # Cantidad de neuronas en la capa oculta
+FINAL_LAYER = 10 # cantidad de neuronas en la capa final 
+
+theta_shapes = np.array([
+    [ HIDDEN_LAYER, n+1 ],
+    [ FINAL_LAYER, HIDDEN_LAYER +1 ]
+])
 
 # generacion de matrices de transicion (aplanadas)
 flat_thetas = flatten_matrixes([
